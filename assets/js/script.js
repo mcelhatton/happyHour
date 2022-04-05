65a087f111bc6ce65fafda192615a781f2ffb3c9
mcelhatton <colinm@mobiledealerdata.com>
2022-04-05 10:27:01 -0500

    saving trivia api console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 
-// OMDb API 
-
+// OMDb API mobie urls / variables
+let searchValue = 'type=movie';
 const movieApiKey = 'a0d72e1';
-const movieApiUrl = `http://www.omdbapi.com/?t=matrix&&apikey=${movieApiKey}`;
-const moviePosterUrl = `http://img.omdbapi.com/?apikey=${movieApiKey}&`;
+const movieApiUrl = `https://www.omdbapi.com/?${searchValue}&&apikey=${movieApiKey}`;
+const moviePosterUrl = `https://img.omdbapi.com/?apikey=${movieApiKey}&`;
+// trivia urls / variables
+const triviaApiUrl = `https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple`;
+const triviaCategoryLookupUrl = `https://opentdb.com/api_category.php`;
 
-fetch(movieApiUrl).then(function(response) {
+fetch(triviaCategoryLookupUrl).then(function(response) {
   response.json().then(function(data) {
     console.log(data);
   });
 });
+
+fetch(triviaApiUrl).then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
+
+// select category
+
+
+// load selected category questions
