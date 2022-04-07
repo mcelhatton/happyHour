9a33a78ecdada353498fa44ba4708a8b624d8f65
mcelhatton <colinm@mobiledealerdata.com>
2022-04-06 19:02:25 -0500

    saving js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,27 @@
 
 // OMDb API mobie urls / variables
-let searchValue = 'type=movie';
+let searchValue = 't=matrix';
 const movieApiKey = 'a0d72e1';
 const movieApiUrl = `https://www.omdbapi.com/?${searchValue}&&apikey=${movieApiKey}`;
-const moviePosterUrl = `https://img.omdbapi.com/?apikey=${movieApiKey}&`;
+
+//const moviePosterUrl = `https://img.omdbapi.com/?apikey=${movieApiKey}&`;
+
+
+let musicGenreUrl ='https://binaryjazz.us/wp-json/genrenator/v1/genre/';
+
+
+// the movie db api
+//const movieApiKey = '42dbe956de7a0a7cd46f2c0cd6110ac2'
+//const movieApiUrl = 'https://api.themoviedb.org/10/movie/550?api_key=42dbe956de7a0a7cd46f2c0cd6110ac2'
+
+
 // trivia urls / variables
 const triviaApiUrl = `https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple`;
 const triviaCategoryLookupUrl = `https://opentdb.com/api_category.php`;
 
-fetch(triviaCategoryLookupUrl).then(function(response) {
+fetch(musicGenreUrl).then(function(response) {
   response.json().then(function(data) {
     console.log(data);
+    }); 
   });
-});
 
-fetch(triviaApiUrl).then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
-
-// select category
-
-
-// load selected category questions
